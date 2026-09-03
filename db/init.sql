1a4cd74eff2756e3525afacfbbee8d1fb51362c4
labubutrue <chimichanga2123837@gmail.com>
2026-09-03 13:17:45 +0300

    Add Docker and Compose setup

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,9 @@
+SET NAMES utf8mb4;
+
 CREATE TABLE items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
-);
-INSERT INTO items (name) VALUES ('Пример 1'), ('Пример 2');
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+INSERT INTO items (name)
+VALUES ('Пример 1'), ('Пример 2');
